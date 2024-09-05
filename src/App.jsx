7ecf4c3ea586ebd9e1870dc3ce42d60ae0f74f1d
saananokela <smnokela@gmail.com>
2024-09-05 20:30:27 +0300

    Improved logic to find selected product and added two extra items to arrays.

--- src/App.jsx
+++ src/App.jsx
@@ -12,15 +12,21 @@ function App() {
     { id: 1, name: 'Smaak', size: 6 },
     { id: 2, name: 'Royal Canin', size:6},
     { id: 3, name: 'Orijen', size:7 },
+    { id: 4, name: 'Acana', size:6 },
+    { id: 5, name: 'Pedigree', size:8 },
   ]
 
-  const selectedProduct = (products.find(product => product.id === selectedProductId)).name
-  // console.log(selectedProduct)
+  const selectedProduct = products.find(product => product.id === selectedProductId);
+  const productName = selectedProduct ? selectedProduct.name : 'Tuntematon tuote';
+  console.log(productName)
+
 
   const productsPrice = [
-    { id: 1, price: '42' },
+    { id: 1, price: '38' },
     { id: 2, price: '51' },
     { id: 3, price: '48' },
+    { id: 4, price: '55' },
+    { id: 5, price: '30' },
   ]
 
 return (
