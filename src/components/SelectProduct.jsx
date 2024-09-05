62e3767e1654e036b3cd6fc50317ea980cb9a684
saananokela <smnokela@gmail.com>
2024-09-05 17:07:57 +0300

    Added getPrice function to display product prices in dropdown.

--- src/components/SelectProduct.jsx
+++ src/components/SelectProduct.jsx
@@ -8,6 +8,18 @@ function SelectProduct() {
     { id: 3, name: 'Orijen' },
   ]
 
+  const productsPrice = [
+    { id: 1, price: '42' },
+    { id: 2, price: '51' },
+    { id: 3, price: '48' },
+  ]
+
+  //haetaan tuotteen hinta, jotta voidaan näyttää se dropdownissa tuotteen perässä
+  const getPrice= (id) => {
+    const productPrice = productsPrice.find(product => product.id === id);
+    return productPrice ? productPrice.price : 'Ei hintaa';
+  };
+
   return (
     <div className='content'>
       <div className='product-select-row'>
@@ -15,7 +27,7 @@ function SelectProduct() {
         <div>
           <select className='product-select'>
             {
-              products.map((product) => <option key={product.id} value={product.id}>{product.name}</option>)}
+              products.map((product) => <option key={product.id} value={product.id}>{product.name} - ({getPrice(product.id)} €) </option>)}
           </select>
         </div>
       </div>
