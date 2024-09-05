52b9bbc27a17f92fc7abdf39fdc6c4edb9082ac2
saananokela <smnokela@gmail.com>
2024-09-05 10:14:13 +0300

    Imported styles for SelecProduct component and imported it into App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import headerImg from './assets/headerImg.png';
 import Header from './components/Header';
+import SelectProduct from './components/SelectProduct';
+
 
 function App() {
 return (
     <div>
       <Header image={headerImg} title={'Welcome to product page!'}/>
+      <SelectProduct />
     </div>
   );
 }
--- src/components/SelectProduct.jsx
+++ src/components/SelectProduct.jsx
@@ -1,7 +1,11 @@
+import '../styles/App.css';
+
 function SelectProduct() {
   return (
     <div>
-
+      <p>Tähän tylee tuotevalikointi</p>
     </div>
   );
 }
+
+export default SelectProduct;
