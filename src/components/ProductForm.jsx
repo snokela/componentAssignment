ea833ef2ba8f478d62f17baacfd0dd239f236ba4
saananokela <smnokela@gmail.com>
2024-09-06 14:43:20 +0300

    Created ProductForm component with basic structure.

---
+++ src/components/ProductForm.jsx
@@ -0,0 +1,11 @@
+function ProductForm() {
+  return (
+    <div>
+      <h3>Select Product</h3>
+      {/* tähän tulee selectproduct component */}
+      {/* tähän tulee selectquantity component */}
+      <h3>Order info</h3>
+      {/* /{* tähän tulee orderinfo component */}
+    </div>
+  )
+}
