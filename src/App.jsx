bb55b72eff1c40676d9291ae850ad2ad0057c20e
saananokela <smnokela@gmail.com>
2024-10-08 15:32:50 +0300

    Added router settings and defined RouterProvider.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import ComponentUI from './components/ComponentUI';
 import OpenDataUI from './components/OpenDataUI';
+
+const myRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <ComponentUI />
+  },
+  {
+    path: '/randomDog',
+    element: <OpenDataUI />
+  },
+]);
 
 function App() {
   return (
     <div>
-      <ComponentUI />
-      <OpenDataUI />
+      <RouterProvider router={myRouter} />
+      {/* <ComponentUI />
+      <OpenDataUI /> */}
     </div>
   );
 }
