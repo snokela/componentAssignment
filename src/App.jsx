1a5ea47ed8d6dc6e3a9b85a5e4539a4d2724d865
saananokela <smnokela@gmail.com>
2024-10-08 15:55:13 +0300

    Added NavigationBar with links to ComponentUI and OpenDataUI pages.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, Link, Outlet, RouterProvider, } from 'react-router-dom';
 import ComponentUI from './components/ComponentUI';
 import OpenDataUI from './components/OpenDataUI';
+
+
+const NavigationBar = () => {
+  return (
+    <div>
+      <Link to={'/'}>Component assignment</Link>
+      <Link to={'/randomDog'}>Open data assignment</Link>
+      <Outlet />
+    </div>
+  );
+}
 
 const myRouter = createBrowserRouter([
   {
     path: '/',
-    element: <ComponentUI />
-  },
-  {
-    path: '/randomDog',
-    element: <OpenDataUI />
+    element: <NavigationBar />,
+    children: [
+      {
+        path: '/',
+        element: <ComponentUI />
+      },
+      {
+        path: '/randomDog',
+        element: <OpenDataUI />
+      },
+    ]
   },
 ]);
 
@@ -24,4 +41,5 @@ function App() {
 }
 
 export default App;
+
 
